798058981618a3e25c733caeabd474191f4d3442
benciitoo <bence.banszegi@gmail.com>
2019-01-22 16:59:49 +0100

    matchmaking

---
+++ week-02/day-2/src/Matchmaking.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Matchmaking {
+    public static void main(String... args) {
+        ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Claire", "Kat", "Jane"));
+        ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Tom", "Todd", "Neef", "Jeff"));
+
+        // Write a method that joins the two lists by matching one girl with one boy into a new list
+        // If someone has no pair, he/she should be the element of the list too
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+
+        System.out.println(makingMatches(girls, boys));
+    }
+
+
+    public static ArrayList<String> makingMatches(ArrayList<String> girls, ArrayList<String> boys) {
+
+        ArrayList<String> matched = new ArrayList<String>();
+        for (int i = 0; i < boys.size(); i++) {
+
+            if (i < girls.size()) {
+                matched.add(girls.get(i));
+            }
+
+            if (i < boys.size()) {
+                matched.add(boys.get(i));
+            }
+
+        }
+        return matched;
+    }
+    
+}
