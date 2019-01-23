e3933771cf427ff590b728cb3d2e01091099167d
benciitoo <bence.banszegi@gmail.com>
2019-01-23 14:30:20 +0100

    purplesteps3d

---
+++ week-02/day-3/src/PurpleSteps3d.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3d {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+
+        int startPos = 10;
+        for (int i = 10; i < 70; i+=10) {
+            graphics.setColor(new Color(128, 0, 128));
+            graphics.fillRect(startPos, startPos, i,i);
+            graphics.setColor(Color.black);
+            graphics.drawRect(startPos, startPos, i,i);
+            startPos += i;
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
