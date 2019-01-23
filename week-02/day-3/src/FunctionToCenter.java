994c9e94387917c990a51ee0207b78ada2af9b10
benciitoo <bence.banszegi@gmail.com>
2019-01-23 15:19:08 +0100

    FunctionToCenter

---
+++ week-02/day-3/src/FunctionToCenter.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Fill the canvas with lines from the edges, every 20 px, to the center.
+
+        int maxAndMin = 0;
+        int backward = WIDTH;
+
+        for (int i = 0; i <= WIDTH; i+=20) {
+            rajzol(maxAndMin, i, graphics);
+            rajzol(WIDTH, backward, graphics);
+            backward -= 20;
+        }
+
+    }
+
+
+    public static void rajzol (int x, int y, Graphics graphics){
+        int center = WIDTH/2;
+        graphics.drawLine(y,x, WIDTH/2, WIDTH/2);
+        graphics.drawLine(x,y, WIDTH/2, WIDTH/2);
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
