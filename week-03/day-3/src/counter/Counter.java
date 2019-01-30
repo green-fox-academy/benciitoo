5e2fac41e414dc9f5c5d2ce485d7aa260898d06e
benciitoo <bence.banszegi@gmail.com>
2019-01-30 12:52:54 +0100

    Counter

---
+++ week-03/day-3/src/counter/Counter.java
@@ -0,0 +1,38 @@
+package counter;
+
+public class Counter {
+
+    int value;
+    int startingValue;
+
+    public Counter(){
+        startingValue = value;
+    }
+
+    public Counter(int valueNumber){
+        value = valueNumber;
+        startingValue = value;
+
+    }
+
+
+    public void add(int addValue){
+        value += addValue;
+    }
+
+
+    public void add(){
+        value += 1;
+    }
+
+
+    public int get(){
+        return value;
+    }
+
+
+    public void reset(){
+        value = startingValue;
+    }
+
+}
