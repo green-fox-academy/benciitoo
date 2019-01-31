99b0a2bcee91b92e1505ac3a7ecf080ecbabe093
benciitoo <bence.banszegi@gmail.com>
2019-01-31 14:44:26 +0100

    Refactorio

---
+++ week-03/day-4/src/Refactorio.java
@@ -0,0 +1,20 @@
+public class Refactorio {
+// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+    public static void main(String[] args) {
+
+        System.out.println(refactorio(6));
+    }
+
+
+    static int refactorio(int number){
+
+        if (number == 1){
+            return number;
+        } else {
+            return (number * (refactorio(number -1)));
+        }
+    }
+    
+}
