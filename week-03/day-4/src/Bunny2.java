6a5a65a7fcd50b54e8e06e5802d048cd4354385e
benciitoo <bence.banszegi@gmail.com>
2019-01-31 14:29:14 +0100

    bunny2

---
+++ week-03/day-4/src/Bunny2.java
@@ -0,0 +1,22 @@
+public class Bunny2 {
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+    public static void main(String[] args) {
+        System.out.println(countTheEarsOfBunnies(8));
+    }
+
+
+    static int countTheEarsOfBunnies(int numberOfBunnies){
+        if (numberOfBunnies == 0){
+            return 0;
+        } else if (numberOfBunnies % 2 == 1) {
+            return (2 + countTheEarsOfBunnies((numberOfBunnies) - 1));
+        } else {
+            return (3 + countTheEarsOfBunnies((numberOfBunnies) - 1));
+        }
+    }
+
+}
---
+++ week-03/day-4/src/String1.java
@@ -0,0 +1,21 @@
+public class String1 {
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+ /*   public static void main(String[] args) {
+
+        String valami = "gzdvxyyzXfxyf";
+        String uj = (valami = valami.substring(0,4) + 'T' + valami.substring(5,valami.length()));
+        System.out.println(uj);
+    }
+
+
+
+
+    changeChar(String valami){
+        if ()
+
+
+    }*/
+
+}
