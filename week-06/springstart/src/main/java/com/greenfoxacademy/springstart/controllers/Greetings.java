2b70694fff0defbeb0a77460c6707241a125f4dd
benciitoo <bence.banszegi@gmail.com>
2019-02-26 14:54:51 +0100

    Exercise 5-6

--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greetings.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greetings.java
@@ -5,12 +5,10 @@ public class Greetings {
     private long id;
     private String content;
 
-
     public Greetings(long id, String content) {
         this.id = id;
         this.content = content;
     }
-
 
     public long getId() {
         return id;
--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,19 +1,22 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 
 public class HelloRESTController {
 
+    private static AtomicLong counter = new AtomicLong(1);
 
     @RequestMapping(value="/greeting")
     @ResponseBody
-    public static Greetings greeting(){
-        Greetings newGreeting = new Greetings(1, "Hello, World!");
+    public static Greetings greeting(@RequestParam ("name") String content){
+        Greetings newGreeting = new Greetings(counter.getAndIncrement(), "Hello, " + content);
         return newGreeting;
     }
 }
