64f3779f12a185b5edce01b5ffcf7511b701f001
benciitoo <bence.banszegi@gmail.com>
2019-04-04 12:09:47 +0200

    firefox test passed

--- week-10/day-4/proba3maventest/src/main/java/Main.java
+++ week-10/day-4/proba3maventest/src/main/java/Main.java
@@ -1,7 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,7 +14,7 @@ public class Main {
 
     private static WebDriver driver = new FirefoxDriver();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         getGoogle();
         searchInGoogle();
         //closeBrowser();
@@ -26,13 +28,17 @@ public class Main {
         driver.close();
     }
 
-    public static void searchInGoogle() {
+    public static void searchInGoogle() throws Exception{
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.name("q")).sendKeys("Süsü");
-        WebElement googleSearchButton = driver.findElement(By.name("btnK"));
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOf(googleSearchButton));
-        googleSearchButton.click();
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        WebElement logo = driver.findElement(By.id("logo"));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(logo));
+
+        //WebElement googleSearchButton = driver.findElement(By.name("btnK"));
+        //new WebDriverWait(driver, 10)
+        //        .until(ExpectedConditions.visibilityOf(googleSearchButton));
+        //googleSearchButton.click();
     }
 
     public static String getTitle(){
--- week-10/day-4/proba3maventest/src/test/java/TestSearchResult.java
+++ week-10/day-4/proba3maventest/src/test/java/TestSearchResult.java
@@ -9,63 +9,134 @@ public class TestSearchResult {
         Main.getGoogle();
     }
 
-    @After
-    public void testShutdown() {
+    @AfterClass
+    public static void testShutdown()  {
         Main.closeBrowser();
     }
 
     @Test
-    public void testSearch1() {
-        //Main.getGoogle();
-
-        Main.searchInGoogle();
-
-        assertEquals("Süsü - Google-keresés", Main.getTitle());
-       // Main.closeBrowser();
-
-    }
-
-
-    @Test
-    public void testSearch2() {
-       // Main.getGoogle();
-
-        Main.searchInGoogle();
-
-        assertEquals("Süsü - Google-keresés", Main.getTitle());
-       // Main.closeBrowser();
-
-    }
-
-   /* @Test
-    public void testSearch3() {
-        Main.searchInGoogle();
-        assertEquals("Süsü - Google-keresés", Main.getTitle());
-    }
-    @Test
-    public void testSearch4() {
+    public void testSearch01() throws Exception{
         Main.searchInGoogle();
         assertEquals("Süsü - Google-keresés", Main.getTitle());
     }
 
     @Test
-    public void testSearch5() {
+    public void testSearch02() throws Exception{
         Main.searchInGoogle();
         assertEquals("Süsü - Google-keresés", Main.getTitle());
     }
 
     @Test
-    public void testSearch6() {
+    public void testSearch03() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+    @Test
+    public void testSearch04() throws Exception{
         Main.searchInGoogle();
         assertEquals("Süsü - Google-keresés", Main.getTitle());
     }
 
     @Test
-    public void testSearch7() {
+    public void testSearch05() throws Exception{
         Main.searchInGoogle();
         assertEquals("Süsü - Google-keresés", Main.getTitle());
     }
-*/
+
+    @Test
+    public void testSearch06() throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch07()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch08()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch09()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+    @Test
+    public void testSearch10() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch11() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch12() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch13()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch14()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch15()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch16()throws Exception {
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch17() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch18() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch19() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+    @Test
+    public void testSearch20() throws Exception{
+        Main.searchInGoogle();
+        assertEquals("Süsü - Google-keresés", Main.getTitle());
+    }
+
+
+
+
+
+
 
 
 }
